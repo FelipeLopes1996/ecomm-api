2b211a77a6e61dfbbf4c98254eec69bb327941ba
Felipe <felipel.lima1996@gmail.com>
2023-07-23 10:05:56 -0300

    add reset and forgot password services, controllers and routes

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,62 @@
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+import nodemailer from 'nodemailer';
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ITemplateVariable {
+  [key: string]: string | number | boolean;
+}
+
+interface IParseMailTemplate {
+  file: string;
+  variables: ITemplateVariable;
+}
+
+interface ISendMail {
+  to: IMailContact;
+  from?: IMailContact;
+  subject: string;
+  templateData: IParseMailTemplate;
+}
+
+export default class EtherealMail {
+  static async sendMail({
+    to,
+    from,
+    subject,
+    templateData,
+  }: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const mailTemplate = new HandlebarsMailTemplate();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+    const message = await transporter.sendMail({
+      from: {
+        name: from?.name || 'Equpe API Vendas',
+        address: from?.email || 'vai@mail',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parse(templateData),
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,49 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
+
+interface IUser {
+  email: string;
+}
+
+class SendForgotPasswordEmailService {
+  public async execute({ email }: IUser): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exist');
+    }
+
+    const { token } = await userTokensRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
+    await EtherealMail.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: 'api vendas',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
